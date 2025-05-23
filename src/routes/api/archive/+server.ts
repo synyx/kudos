e02abaa7627acc5483a0da09b5a6ef2f377b0cdd
enoy19 <enoy19+github@gmail.com>
2025-05-23 15:13:17 +0200

    updated api to drizzle

--- src/routes/api/archive/+server.ts
+++ src/routes/api/archive/+server.ts
@@ -1,20 +1,17 @@
-import { PrismaClient } from '@prisma/client';
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-
-const prisma = new PrismaClient();
+import { db } from '$lib/server/db';
+import { kudos } from '$lib/server/db/schema';
+import { inArray } from 'drizzle-orm';
 
 export const POST: RequestHandler = async ({ request }) => {
-  const kudoIds = await request.json();
+	const kudoIds = await request.json();
 
-  try {
-    await prisma.kudo.updateMany({
-      data: { archived: true },
-      where: { id: { in: kudoIds } },
-    });
-  } catch (e) {
-    error(500, `couldn't archive kudos: ${e}`);
-  }
+	try {
+		await db.update(kudos).set({ archived: true }).where(inArray(kudos.id, kudoIds)).execute();
+	} catch (e) {
+		error(500, `couldn't archive kudos: ${e}`);
+	}
 
-  return new Response('archived');
+	return new Response('archived');
 };
