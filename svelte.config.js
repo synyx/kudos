bde83942bf997ae853e173568ee83350ba3150a6
enoy19 <enoy19+github@gmail.com>
2025-05-23 18:03:38 +0200

    refactor: update Playwright and Vite configurations, add Vitest setup for testing

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,9 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte'],
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true,
-    }),
-  ],
-
-  vitePlugin: {
-    inspector: true,
-  },
-  kit: {
-    adapter: adapter(),
-  },
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
+
 export default config;
